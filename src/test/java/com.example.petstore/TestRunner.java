809f52b922877320213bbbdc0c00e9ad24946e90
JosieWonders <josiewonders@proton.me>
2025-03-09 14:44:25 +0100

    update for maven

--- src/test/java/com.example.petstore/TestRunner.java
+++ src/test/java/com.example.petstore/TestRunner.java
@@ -1,6 +1,13 @@
 package com.example.petstore;
 
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
+@Suite
+@SelectClasses({
+        CreatePetTest.class,
+        GetPetTest.class,
+        UpdatePetTest.class
+})
 public class TestRunner {
 }
